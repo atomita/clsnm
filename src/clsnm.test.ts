6c8586db0501cce46fce69c2e6c8cde1e0caa678
atomita <sleeping.cait.sith+gh@gmail.com>
2019-07-02 21:36:54 +0900

    test: Add toKebab

--- src/clsnm.test.ts
+++ src/clsnm.test.ts
@@ -3,28 +3,32 @@ import * as assert from 'power-assert'
 import clsnm from './clsnm'
 
 test('', () => {
-  const data = [
-    ['', {}],
+  const toKebab = (v: string) => v.replace(/[A-Z]/g, (v) => '-' + v.toLowerCase())
+  const data: [string, any, (undefined|((v: string) => string))][] = [
+    ['', {}, undefined],
     [
       'success',
       {
         fail: false,
         success: true
-      }
+      },
+      undefined
     ],
     [
       'join success',
       {
         join: true,
         success: true
-      }
+      },
+      undefined
     ],
 
     [
       'foo',
       {
         foo: {}
-      }
+      },
+      undefined
     ],
     [
       'foobar',
@@ -32,7 +36,8 @@ test('', () => {
         foo: {
           bar: true
         }
-      }
+      },
+      undefined
     ],
     [
       'foo',
@@ -40,22 +45,46 @@ test('', () => {
         foo: {
           bar: false
         }
-      }
+      },
+      undefined
     ],
     [
       'foo_active foo_bar-baz',
       {
-        'foo': {
+        foo: {
           _active: true,
           '_bar-baz': true,
           _inactive: false
         }
-      }
+      },
+      undefined
+    ],
+    [
+      'foo_active foo_bar-baz',
+      {
+        foo: {
+          _active: true,
+          _barBaz: true,
+          _inactive: false
+        }
+      },
+      toKebab
+    ],
+    [
+      'foo-bar-baz',
+      {
+        foo: {
+          Bar: {
+            Baz: true
+          }
+        }
+      },
+      toKebab
     ],
   ]
 
-  data.map(([result, obj]) => {
-    assert(result === clsnm(obj))
+  data.map(([result, obj, mapper]) => {
+    assert(result === clsnm(obj, mapper))
   })
 
 })
